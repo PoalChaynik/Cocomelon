4ca5ca4e7f1a935184a1b0cb0e8d0e9278a138c2
PoalChaynik <alpolaop13@gmail.com>
2022-09-12 11:47:28 +0300

    12.09.2022

--- oop.py
+++ oop.py
@@ -15,4 +15,13 @@ class Auto():
         self.krasa = krasa
 
     def dati(self):
-        
+        print(f'Modelis ir {self.modelis}, krasa - {self.krasa}')
+    
+    def krasas_maina(self, jaunaKrasa):
+        print(f'\nIepriekseja auto krasa - {self.krasa}')
+        print(f'Jauna auto krasa - {jaunaKrasa}')
+
+audi = Auto(modelis='Q6',krasa='Sudraba')
+
+audi.dati()
+audi.krasas_maina('Balta')
