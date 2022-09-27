ef9f7b9421b56c454c8a8fdfa4e3be4afb89375a
PoalChaynik <alpolaop13@gmail.com>
2022-09-27 12:58:33 +0300

    27.09.2022

--- Images/pil.py
+++ Images/pil.py
@@ -1,13 +1,31 @@
 from PIL import Image
 
-# DoggieImage = Image.open('Doggie.jpg')
-# DoggieImage.load()
+def bilde():
+    datne = 'Images/bilde.jpg'
+    with Image.open(datne) as im:
 
-try:
-    DoggieImage = Image.open('Images\Doggie.jpg')
-except FileNotFoundError:
-    print('File Not Found!')
-DoggieImage.load()
+        print(datne, im.format, f"{im.size}x{im.mode}")
+        im.show()
+        izmers = (100, 100)
+        im.thumbnail(izmers)
+        im.save("Images/bilde-maza.jpg", im.format)
+        im.show()
+bilde()
 
-print(DoggieImage.format, DoggieImage.size, DoggieImage.mode)
-DoggieImage.show()
+def pagriezt(nosaukums):
+    with Image.open(f'Images/{nosaukums}') as img:
+        img = img.rotate(90)
+        img.save(f'Images/cits-{nosaukums}', img.format)
+
+pagriezt('cj.jpg')
+
+def sikatels(name,pikseli):
+    izmers = pikseli.split(',')
+    x = int(izmers[0])
+    y = int(izmers[1])
+    izmeers = (x,y)
+    with Image.open(f'Images/{name}') as image:
+        image.thumbnail(izmeers)
+        image.save(f'Images/Izmainits_{name}', image.format)
+
+sikatels('cj.jpg','100,100')
